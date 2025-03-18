e8b9ff659f71ccecbf3bf682af15509df3591147
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-18 17:55:38 +0200

    Create fileHandler.js

---
+++ TMDB_CLI/utils/fileHandler.js
@@ -0,0 +1,42 @@
+import { access, constants, readFile, writeFile } from 'node:fs';
+
+const fileExist = (file, callback) => {
+  access(file, constants.F_OK, (err) => {
+    if (err) {
+      console.log('File does not Exist, Creating New File .....');
+      return callback(false);
+    }
+    callback(true);
+  });
+};
+
+const fileWrite = (file, content) => {
+  writeFile(file, JSON.stringify(content), 'utf-8', (err) => {
+    if (err) {
+      console.log(`Error in Create/Add Task:\n ${err}`);
+      process.exit();
+    }
+  });
+};
+
+const fileRead = (file, callback) => {
+  readFile(file, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(`Error in Read File:\n ${err}`);
+      callback([]);
+    }
+    let retData;
+    try {
+      retData = JSON.parse(data);
+      if (!Array.isArray(retData)) retData = [];
+    } catch (err) {
+      console.log(
+        `File Content is Not valid JSON. Initialiaing as empty array.`
+      );
+      retData = [];
+    }
+    callback(retData);
+  });
+};
+
+export { fileExist, fileWrite, fileRead };
